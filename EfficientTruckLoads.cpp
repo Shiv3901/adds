4c940005555d3bd3b3bff501c38b3cf7b7e5e1c6
Shivam Savani <shivambsavani@gmail.com>
2021-04-02 23:45:56 +1030

    sdg

--- EfficientTruckLoads.cpp
+++ EfficientTruckLoads.cpp
@@ -8,11 +8,9 @@ EfficientTruckloads::EfficientTruckloads() {
 }
 
 int EfficientTruckloads::numTrucks(int n1, int n2) {
-    if (count == 0) {
-        if ( (n1 < 2 || n1 > 10000) || (n2 < 1 || n2 > n1 - 1) ) {
-            return -1;
-        }
-        count++;
+    
+    if ( (n1 < 2 || n1 > 10000) || (n2 < 1 || n2 > n1 - 1) ) {
+        return -1;
     }
     
     if (dp[n1] != 0) return dp[n1];
@@ -23,7 +21,7 @@ int EfficientTruckloads::numTrucks(int n1, int n2) {
     }
 
     if (n1 % 2 == 0) {
-        dp[n1] = 2*numTrucks(n1/2, n2);
+        dp[n1] = 2 *  numTrucks(n1/2, n2);
         return dp[n1];
     }
     else {
