ff551a14f95f0d75204504884956742e48f023d4
Shivam Savani <shivambsavani@gmail.com>
2021-04-02 23:50:43 +1030

    done

--- TruckLoads.cpp
+++ TruckLoads.cpp
@@ -7,7 +7,7 @@ Truckloads::Truckloads() {
 int Truckloads::numTrucks(int n1, int n2) {
 
 	if (count == 0) {
-		if ( (n1 < 2 || n1 > 10000) || (n2 < 1 || n2 > n1 - 1) ) {
+		if ( (n1 < 2) || (n2 < 1) ) {
 			return -1;
 		}
 		count++;
