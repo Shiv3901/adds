f4eb6760755ff34dda571fdff8597317d1a3bcb1
Shivam Savani <shivambsavani@gmail.com>
2021-05-14 02:13:52 +0930

    sdihyg

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,9 +5,9 @@ BubbleSort::BubbleSort() {}
 
 vector<int> BubbleSort::sort(vector<int>& list) {
 
-	for (int i = 0 ; i < list.size() ; i++) {
+	for (int i = 0 ; i < int(list.size()) ; i++) {
 
-		for (int j = 0 ; j < list.size()-i-1 ; j++) {
+		for (int j = 0 ; j < int(list.size()-i-1) ; j++) {
 
 			if (list[j] > list[j+1]) {
 				int temp = list[j];
